25ed1457ec58d61b887929a4c0b1cd897cbb1b6a
Ihor Nazarkin <ihornazarkin@gmail.com>
2026-02-28 12:43:19 +0000

    third test added

--- tests/test_first_search.py
+++ tests/test_first_search.py
@@ -10,3 +10,8 @@ def test_hamburger_menu(logged_in_page):
     page.get_by_role("button", name="Open Menu").click()
     expect(page.get_by_role("link", name="About")).to_be_visible()
 
+def test_logout_is_visible(logged_in_page):
+    page = logged_in_page
+    page.get_by_role("button", name="Open Menu").click()
+    page.locator("[data-test=\"logout-sidebar-link\"]").click()
+    expect(page.locator("[data-test=\"login-button\"]")).to_be_visible()
